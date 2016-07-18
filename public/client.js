a34a9b6bd23cec37903f47860c3e9154d57c6f3a
Connor O'Gara <ogara.connor@gmail.com>
2016-07-18 15:26:31 -0500

    add notes

--- public/client.js
+++ public/client.js
@@ -1,29 +1,37 @@
+//create angular app
 angular.module('angularCodeChallenge', []);
-
+//create controller
 angular.module('angularCodeChallenge').controller('MainController', MainController);
-
+//controller function
 function MainController($http) {
+  //set view model to vm
   var vm = this;
 
-  vm.test = 'Testing Angular';
+  //set default button text to 'Display'
   vm.toggle = 'Display';
+  //assign getNames function to vm object
   vm.getNames = getNames;
+  //initially hide students list
   vm.show = false;
 
   function getNames() {
+    //check if request has been done before, if so, skip request.
     if (typeof vm.students != 'object') {
     $http.get('/students').then(handleSuccess, handleFailure);
     }
+    //change button to display 'Hide' if students are shown
     if (vm.show === false) {
       vm.show = true;
       vm.toggle = 'Hide'
     }
+    //change button to display 'Display' if students are hidden
     else {
       vm.show = false;
       vm.toggle = 'Display'
     }
   }
 
+  //set vm.students as students array
   function handleSuccess(request, response) {
     vm.students = request.data;
     console.log('Students received!');
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+//students array
 var students = [
   "Andrew Kolander",
   "Anthony Maki",
@@ -24,13 +25,16 @@ var students = [
   "Trent Johnson"
 ];
 
+//static files
 app.use(express.static('public'));
 
+//send student array to client when requested
 app.get('/students', function(request, response) {
   console.log('--- Sending Students Array: ---\n', students, '\n --- END OF ARRAY ---');
   response.send(students);
 })
 
+//server listen
 var server = app.listen(process.env.PORT || 3000, function() {
   var port = server.address().port;
   console.log('Listening on port', port);
